9acfb000a85efd650ccf1754311e24a68136e465
Julian Maurice <julian.maurice@biblibre.com>
2017-01-18 09:14:46 +0100

    Change jQuery selector (use div instead of fieldset)

--- asset/js/idref.js
+++ asset/js/idref.js
@@ -1,6 +1,6 @@
 (function($) {
     $(document).ready(function() {
-        $('body').on('focus', 'fieldset[data-data-type="idref"] input.value', function() {
+        $('body').on('focus', 'div[data-data-type="idref"] input.value', function() {
             if ($(this).autocomplete('instance') === undefined) {
                 $(this).autocomplete({
                     "source": $(this).attr('data-url'),
